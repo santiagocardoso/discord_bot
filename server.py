58b1d51e72e72197ee30636306bf89ddc55ee506
Santiago Cardoso <104155579+santiagocardoso@users.noreply.github.com>
2023-03-20 23:29:51 -0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+  return "Oie, estou on!"
+
+def run():
+  app.run(host='0.0.0.0', port=8000)
+
+def server():
+  t = Thread(target=run)
+  t.start()
+  
